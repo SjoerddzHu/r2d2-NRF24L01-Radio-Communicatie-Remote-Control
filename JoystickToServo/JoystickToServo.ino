e7b47986389a6219700b6344f3ed830d70b0579e
SjoerddzHu <31765239+SjoerddzHu@users.noreply.github.com>
2019-09-01 17:01:01 +0200

    r2d2-NRF24L01-Radio-Communicatie-Remote-Control
    Needs RF24_library to work

---
+++ JoystickToServo/JoystickToServo.ino
@@ -0,0 +1,69 @@
+
+#include <Servo.h>
+
+Servo ForeAft;    // Define a Servo for Forward and Reverse Motion
+Servo LeftRight; // Define a Servo for Left and Right Motion
+
+int ForeAft_Pin = 0;
+int LeftRight_Pin = 1;
+
+float ForeAft_Input ;       // Range
+float LeftRight_Input ;    // Range
+float ForeAft_Output;       // Range 0 - 180 degrees
+float LeftRight_Output;    // Range 0 - 180 degrees
+int Fore_Limit = 800;       // High ADC Range of Joystick ForeAft
+int Aft_Limit = 220;       // Low ADC Range of Joystick ForeAft
+int Right_Limit = 800;     // High ADC Range of Joystick LeftRight
+int Left_Limit = 226;      // Low ADC Range of Joystick LeftRight
+
+void setup()               // Main Program Initialization
+{
+  Serial.begin(9600);
+
+  ForeAft.attach(12);       // Servo Output 
+  LeftRight.attach(13);    //  Servo Output
+
+}
+
+void loop()
+{
+  ForeAft_Input = analogRead(ForeAft_Pin) ;
+  LeftRight_Input = analogRead(LeftRight_Pin) ;
+  ForeAft_Output = convertToServo(ForeAft_Input) ;  
+  LeftRight_Output = convertToServo(LeftRight_Input) ;
+
+    ForeAft.write(ForeAft_Output);
+    LeftRight.write(LeftRight_Output);
+
+
+  //  Serial.print(LeftRight_Input);
+  //  Serial.print(" ");
+  Serial.print(ForeAft_Output);
+  Serial.print(" ");
+  Serial.print(LeftRight_Output);
+  Serial.print(" ");
+
+  Serial.println();
+
+}
+
+
+int convertToServo(float x){    // Function to convert between ADC and Servo Value
+  float result;
+  result = ((x-220)*0.31);
+  return round(result);
+}
+
+int convertForeAftToServo(float y){
+  int result;
+ 
+  result = ((y-Aft_Limit)/(Fore_Limit-Aft_Limit)*180);
+  return result;
+}
+
+int convertLeftRightToServo(float x){
+  int result;
+  
+  result = ((x-Left_Limit)/(Right_Limit-Left_Limit)*180);
+  return result;
+}
---
+++ RF24toServo/RF24toServo.ino
@@ -0,0 +1,49 @@
+/*  Code to receive data from RF24L01+ and use it to control a servo */
+
+#include <Servo.h>
+#include <SPI.h>
+#include <RF24.h>
+
+//Radio Configuration
+bool radioNumber=0;
+RF24 radio(9,10);
+byte addresses[][6] = {"1Node","2Node"};
+bool role = 0;  //Control transmit/receive
+
+// Create variables to control servo value
+
+Servo ForeAft;
+Servo LeftRight;
+unsigned int ForeAft_Output;       // Expected range 0 - 180 degrees
+unsigned int LeftRight_Output;     // Expected range 0 - 180 degrees
+
+void setup() {
+
+  Serial.begin(9600);       // Send data back for debugging purposes
+  ForeAft.attach(6);         // Plug a servo signal line into digital output pin 6
+  LeftRight.attach(7);       // Plug a servo signal line into digital output pin 7
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+
+  radio.startListening();  
+}
+
+void loop() {
+
+  delay(100);
+  if(radio.available()){
+         radio.read(&ForeAft_Output,sizeof(ForeAft_Output));
+         radio.read(&LeftRight_Output,sizeof(ForeAft_Output));
+  } else {Serial.print("No radio");
+  }
+  Serial.print(ForeAft_Output);
+  Serial.print("\t");
+  Serial.println(LeftRight_Output);
+  
+  ForeAft.write(ForeAft_Output);                          // Command the Fore/Aft servo to a position
+  LeftRight.write(LeftRight_Output);                    // Command the Left/Right servo to a position
+
+}
